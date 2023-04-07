4d462aa7e08caca81df790d1d9ecb5a8db44b352
Stefan Simon <stefan.j.c.simon@gmail.com>
2023-04-07 14:41:18 +0200

    [*] new_func to add_2

--- some.js
+++ some.js
@@ -1,7 +1,7 @@
 const addition = x => y => z => x + y + z;
 
-const newfunc = addition(2);
-const newfunc2 = newfunc(3);
+const add_2 = addition(2);
+const newfunc2 = add_2(3);
 const result = newfunc2(5);
 
 console.log(result);

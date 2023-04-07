8ee65b27103f72c9852f375a5544720f1e64a097
Stefan Simon <stefan.j.c.simon@gmail.com>
2023-04-07 14:40:37 +0200

    [*] somefunc to addition

--- some.js
+++ some.js
@@ -1,6 +1,6 @@
-const somefunc = x => y => z => x + y + z;
+const addition = x => y => z => x + y + z;
 
-const newfunc = somefunc(2);
+const newfunc = addition(2);
 const newfunc2 = newfunc(3);
 const result = newfunc2(5);
 

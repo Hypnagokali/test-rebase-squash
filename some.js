fa40c343699bffb13216735e8e27953899cd62b8
Stefan Simon <stefan.j.c.simon@gmail.com>
2023-04-07 14:40:37 +0200

    [*] refactoring function names

--- some.js
+++ some.js
@@ -1,7 +1,7 @@
-const somefunc = x => y => z => x + y + z;
+const addition = x => y => z => x + y + z;
 
-const newfunc = somefunc(2);
-const newfunc2 = newfunc(3);
-const result = newfunc2(5);
+const add_2 = addition(2);
+const add_3 = add_2(3);
+const result = add_3(5);
 
 console.log(result);
